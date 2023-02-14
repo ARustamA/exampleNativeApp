0cbca951ba0e6ed1d5752cc8239ebf51fa36cb63
“ARustamA” <rakhmetyanov@gmail.com>
2023-02-14 16:15:05 +0600

    news

---
+++ components/Main.js
@@ -0,0 +1,17 @@
+import { StyleSheet, Text, View } from "react-native";
+import { styles } from "../styles/style";
+
+
+export default function Main() {
+
+   return (
+      <View
+         style={styles.main}>
+         <Text style={styles.text}>Main page</Text>
+
+      </View>
+   );
+}
+const style = StyleSheet.create({
+
+})
---
+++ styles/style.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+   main: {
+      flex: 1,
+      backgroundColor: "gray",
+      alignItems: 'center',
+      justifyContent: 'center',
+      padding: 30
+   },
+   text: {
+      color: 'white',
+      fontFamily: 'mt-bold',
+      fontSize: 20
+   }
+})
